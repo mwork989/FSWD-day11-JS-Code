7b53cceec0ad48c928d7aa4aa73c2fe584b25cc3
unknown <maheshwork989@gmail.com>
2023-12-10 19:16:42 +0530

    class code

--- README.md
+++ README.md
@@ -5,3 +5,17 @@ reference documents
 
 Boostrap adjustment classes -e.g. mt
 https://getbootstrap.com/docs/4.0/utilities/spacing/#horizontal-centering
+
+
+installing the json server command
+-------------
+npm install -g json-server
+
+
+this location the json server is getting stored
+
+C:\Users\<username>\AppData\Roaming\npm\node_modules\json-server
+
+start the json server using this command
+-----------
+json-server --watch db.json
--- script.js
+++ script.js
@@ -4,22 +4,115 @@ let foodDetails = [
         id:2,
         name:'Burger',
         description:' Delicious burger with cheese and veggies',
-        price:250
+        price:250,
+        image:"https://wallpaperaccess.com/thumb/715621.jpg"
      }, {
         id:3,
         name:'Dosa',
         description:'Delicious Dosa with chutney and sambar',
-        price:150
+        price:150,
+        image:"https://wallpaperaccess.com/thumb/6340448.jpg"
      },
      {
         id:1,
         name:'Pizza',
         description:'Classic pizza with your favorite toppings',
-        price:300
+        price:300,
+        image:"https://wallpaperaccess.com/full/3667703.jpg"
+     },
+     {
+        id:4,
+        name:'Donut',
+        description:'sweet and cholatey donut',
+        price:120,
+        image:"https://wallpaperaccess.com/thumb/1636234.jpg"
+     },  {
+        id:5,
+        name:'idli',
+        description:'savoury rice cake',
+        price:50,
+        image:"https://wallpaperaccess.com/thumb/4883006.jpg"
+     },  {
+        id:6,
+        name:'Pani Puri',
+        description:'Deep-fried breaded sphere with filling',
+        price:40,
+        image:"https://wallpaperaccess.com/full/9450617.jpg"
      }
 ]
 
-let cart = []
+let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+
+function loadAllDetails(){
+    // to fetch food details make an api call
+    getFoodDetailsFromJsonServer();
+    createFoodCards();
+   // createFoodTable();
+}
+
+
+
+function createFoodCards(){
+    const foodContainer = document.getElementById('food-container');
+    //   adding the class name for the container
+    
+    const foodCardRow = document.createElement('div');
+    foodContainer.classList.add('mt-4')
+    foodCardRow.classList.add('row');
+   
+    foodDetails.forEach(item=>{
+        const foodCard = document.createElement('div');
+        foodCard.classList.add('food-card');
+        foodCard.classList.add('mx-auto');
+        foodCard.innerHTML = `
+        
+        <h3 >${item.name}</h3>
+        <p>${item.description}</p>
+        <img class="card-img-top" src=${item.image} alt="Card image cap" />
+        <p>Price: $${item.price}</p>
+        
+       
+        <button onclick="viewItemDetails(${item.id})">Add to Cart</button>
+    `;
+    foodCardRow.appendChild(foodCard);
+
+    })
+    foodContainer.appendChild(foodCardRow);
+}
+
+function createFoodTable(foodDetailsRecords){
+    const foodTableBody = document.querySelector('#food-table tbody');
+    foodDetailsRecords.forEach(item => {
+        // Create a table row for each food item
+        const row = document.createElement('tr');
+
+        // Populate the row with food details
+        row.innerHTML = `
+            <td>${item.name}</td>
+            <td>${item.description}</td>
+           
+            <td><img class="card-img-top" src=${item.image} alt="Card image cap" /></td>
+            <td>$${item.price}</td>
+            <td><button onclick="viewItemDetails(${item.id})">Add to Cart</button></td>
+        `;
+
+        // Append the row to the table body
+        foodTableBody.appendChild(row);
+    });
+   //syntax equivalent for document.getElementById('food-table') in jquery below code we can use
+   $('#food-table').DataTable();
+}
+
+
+//'' "" 
+//es5 format
+// var sureshage =24;
+// var rajeshage=25;
+// "suresh=" +sureshage+"rajesh="+rajeshage
+
+// //es6  template literals
+// `suresh= ${sureshage} rajesh= ${rajeshage}`
 
 function viewItemDetails(foodId){
 
@@ -47,6 +140,11 @@ function  goBack(){
    window.history.back()
 }
 
+function  goHome(){
+    window.location.href = 'index.html'
+   
+ }
+
 function displayItemDetails(){
 
     /////////////////////converting into js object    // getting the value back from key from localstorage
@@ -69,35 +167,78 @@ function displayItemDetails(){
 }
 
 function addToCart(){
-    window.location.href= 'checkout.html'
+ 
     const selectedItem = JSON.parse(localStorage.getItem('selectedItem'));
     // while in the cart we dont need description
-    cart.push({ id: selectedItem.id, name: selectedItem.name, price: selectedItem.price})
-    updateCart();
+    if (selectedItem) {
+        cart.push({ id: selectedItem.id, name: selectedItem.name, price: selectedItem.price });
+        updateCart();
+        saveCartToLocalStorage();
+       window.location.href = 'checkout.html';
+    } else {
+        alert('Item not found!');
+    }
 }
+
+function saveCartToLocalStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+
 
 function updateCart() {
     const cartItemsList = document.getElementById('cart-items');
     const totalElement = document.getElementById('total');
-
+     console.log(cartItemsList,cart)
        // Clear previous items
-       cartItemsList.innerHTML = '';
-       let total = 0;
+       if(cartItemsList){
+        cartItemsList.innerHTML = '';
+        let total = 0;
+        console.log('cart',cart)
+        cart.forEach(item => {
+            const listItem = document.createElement('li');
+            listItem.innerText = `${item.name} - ${item.price}`;
+            cartItemsList.appendChild(listItem);
+            total += item.price;
+        });
+        // Update total price
+        totalElement.innerText = total
+       }
+   
 
         // Display items in the cart
 
         // es5 fromat dynamically prepareing the list item
-    cart.forEach(item => {
-        const listItem = document.createElement('li');
-        listItem.innerText = `${item.name} - $${item.price}`;
-        cartItemsList.appendChild(listItem);
-        total += item.price;
-    });
-    // Update total price
-    totalElement.innerText = total
+  
 }
 
 function checkout() {
     // Dummy function for checkout process
-    alert('Checkout process goes here!');
+
+   // alert('Checkout process goes here!');
+   saveCartToJsonServer();
 }
+
+function saveCartToJsonServer(){
+  //  JSON Server is running on localhost:3000
+  fetch('http://localhost:3000/cart',{
+    method:'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body:JSON.stringify(cart)
+  }).then(response => response.json()).then(data => console.log('Cart saved to server:', data))
+  .catch(error => console.error('Error saving cart to server:', error));
+}
+
+
+function getFoodDetailsFromJsonServer(){
+    fetch('http://localhost:3000/foodDetails',{
+        method:'GET'
+      }).then(response => response.json()).then( foodDetails =>{
+        createFoodTable(foodDetails)
+      }).catch(error => console.error('Error fetching cart items:', error));
+}
+
+
+
